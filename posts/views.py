0a0ddfbb0fcee3f354a6b6f17c8dcb356624262b
ellyjohnmwangi <johnmwangielly@gmail.com>
2020-09-24 00:33:05 +0300

    index,base models update

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+from .models import Post
+
 
 # Create your views here.
+
+class IndexView(ListView):
+    template_name = "posts/index.html"
+    model = Post
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
