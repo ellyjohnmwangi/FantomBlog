f5bbde763057863d2fb8bc2b087a6c4387cb5df6
ellyjohnmwangi <johnmwangielly@gmail.com>
2020-09-24 00:33:05 +0300

    index,base models update

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+
+class AdminPost(admin.ModelAdmin):
+    list_filter = ['publishing_time']
+    list_display = ['title', 'publishing_time']
+    search_fields = ['title', 'content']
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, AdminPost)
