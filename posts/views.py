a1751d521870756b5468b94c4c1a62ca454ac7f1
ellyjohnmwangi <johnmwangielly@gmail.com>
2020-09-25 00:12:50 +0300

    created DetailView for posts model

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Post
 
 
@@ -12,4 +12,14 @@ class IndexView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        return context
+
+
+class PostDetail(DetailView):
+    template_name = 'post/detail.html'
+    model = Post
+    context_object_name = 'single'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
         return context
