54b46dfe9a52e3a6f1cb99cef74cd8abece8fac1
ellyjohnmwangi <johnmwangielly@gmail.com>
2020-09-26 16:17:48 +0300

    category model designed

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.conf import settings
+from django.template.defaultfilters import slugify
 
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
 
 class Post(models.Model):
@@ -11,3 +22,11 @@ class Post(models.Model):
     publishing_time = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(blank=True, null=True, upload_to='uploads/')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    slug = models.SlugField(default="slug")
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
