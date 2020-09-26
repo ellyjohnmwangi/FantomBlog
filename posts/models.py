e0c29669a8cde4dea4728d109bc496a2392a6bd8
ellyjohnmwangi <johnmwangielly@gmail.com>
2020-09-26 23:20:06 +0300

    Categories model update

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=150)
-    slug = models.SlugField()
+    slug = models.SlugField(editable=False)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -15,6 +15,9 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def post_count(self):
+        return self.posts.all().count()
+
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
@@ -22,7 +25,8 @@ class Post(models.Model):
     publishing_time = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(blank=True, null=True, upload_to='uploads/')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    slug = models.SlugField(default="slug")
+    slug = models.SlugField(default="slug", editable=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1, related_name='posts')
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
