4daf203d97152259f126397c218e905669d450bb
ellyjohnmwangi <johnmwangielly@gmail.com>
2020-10-01 00:02:57 +0300

    duplicate post model remove && tag model add

--- posts/models.py
+++ posts/models.py
@@ -36,27 +36,16 @@ class Post(models.Model):
         return self.title
 
 
-class Post(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField()
-    publishing_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(blank=True, null=True, upload_to='uploads/')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    slug = models.SlugField(default="slug", editable=False)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1, related_name="posts")
-    tag = models.ManyToManyField(Tag, related_name="posts", blank=True)
-    slider_post = models.BooleanField(default=False)
-    hit = models.PositiveIntegerField(default=0)
-
-    def comment_count(self):
-        return self.comments.all().count()
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Post, self).save(*args, **kwargs)
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(editable=False)
 
     def __str__(self):
         return self.title
 
-    def post_tag(self):
-        return ','.join(str(tag) for tag in self.tag.all())
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Tag, self).save(*args, **kwargs)
+
+    def post_count(self):
+        return self.posts.all().count()
