6c75c257ecfb71781c670de98fdd033904943204
ellyjohnmwangi <johnmwangielly@gmail.com>
2020-12-31 01:18:11 +0300

    Category Detail class based view created

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from django.views.generic import TemplateView, ListView, DetailView
 
@@ -12,15 +12,31 @@ class IndexView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['categories'] = Category.objects.all()
         return context
 
 
 class PostDetail(DetailView):
     template_name = 'posts/detail.html'
     model = Post
+
     context_object_name = 'single'
 
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data(**kwargs)
+        return context
+
+
+class CategoryDetail(ListView):
+    model = Post
+    template_name = 'categories/category_detail.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        return Post.objects.filter(category=self.category).order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryDetail, self).get_context_data(**kwargs)
+        self.category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        context['category'] = self.category
         return context
