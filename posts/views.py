8e23e03468fb9c2336998b2abbeab336799a14d0
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-03-17 19:04:16 +0300

    save post function added for post creation

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+from django.template.defaultfilters import slugify
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+
 from .models import Post, Category, Tag
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from .forms import PostCreationForm
@@ -60,7 +65,27 @@ class TagDetail(ListView):
         return context
 
 
+@method_decorator(login_required(login_url='users/login'), name='dispatch')
 class CreatePostView(CreateView):
     template_name = 'posts/create-post.html'
     form_class = PostCreationForm
     model = Post
+
+    def get_success_url(self):
+        return reverse('detail', kwargs={"pk": self.object.pk, 'slug': self.object.slug})
+
+    def form_valid(self, form):
+        form.instance.user = self.request.userx
+        form.save()
+
+        tags = self.request.POST.get("tag").split(',')
+
+        for tag in tags:
+            current_tag = Tag.objects.filter(slug=slugify(tag))
+            if current_tag.count() < 1:
+                create_tag = Tag.objects.create(title=tag)
+                form.instance.tag.add(create_tag)
+            else:
+                existed_tag = Tag.objects.get(slug=slugify(tag))
+                form.instance.tag.add(existed_tag)
+        return super(CreatePostView, self).form_valid(form)
