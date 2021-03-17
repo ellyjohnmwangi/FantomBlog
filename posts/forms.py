5f33d379efdc24a4d1ce9116e117d1c1a6850872
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-03-17 16:57:05 +0300

    post creation form style formatting

--- posts/forms.py
+++ posts/forms.py
@@ -6,11 +6,14 @@ class PostCreationForm(forms.ModelForm):
 
     class Meta:
         model = Post
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'single-input', 'placeholder': 'Enter your title here'}),
+            'content': forms.Textarea(attrs={'class': 'single-input', 'placeholder': ' What do you have in mind?'}),
+        }
 
         fields = [
             'title',
             'category',
             'content',
-            'image',
-            'tag'
+            'image'
         ]
