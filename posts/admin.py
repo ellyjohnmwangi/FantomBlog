403ff5a1f4d1347bf83cc1ea1d7c48f78419031a
ellyjohnmwangi <johnmwangielly@gmail.com>
2021-04-13 00:50:37 +0300

    Registered comment view

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 
 # Register your models here.
@@ -12,7 +12,15 @@ class AdminPost(admin.ModelAdmin):
     class Meta:
         model = Post
 
+class AdminComment(admin.ModelAdmin):
+    list_filter = ('publishing_date',)
+    search_fields =  ('name','email','content','post__tittle',)
+
+    class Meta:
+        model = Comment
+
 
 admin.site.register(Post, AdminPost)
 admin.site.register(Category)
 admin.site.register(Tag)
+admin.site.register(Comment,AdminComment)
